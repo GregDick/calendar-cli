ead00b8205298df4cc14182601339192f7f1ea8c
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-07-24 13:28:37 -0500

    added zeller function with test

--- test/unit/reformat.test.js
+++ test/unit/reformat.test.js
@@ -4,6 +4,6 @@ var reformat = require(process.cwd() + '/lib/reformat');
 describe('reformat function', function(){
   it('should return m=13 and y=2014 for january 2015', function(){
     var dat = {day: '1', month: '1', year:'2015'};
-    reformat(dat).should.eql({q:'1', m:'13', k:'14', j:'20'})
+    reformat(dat).should.eql({q:'1', m:'13', y:'2014'})
   })
 })
--- test/unit/zeller.test.js
+++ test/unit/zeller.test.js
@@ -3,6 +3,7 @@ var zeller = require(process.cwd() + '/lib/zeller');
 
 describe('zeller function', function () {
   it('should return the correct day of the week given a day month and year', function(){
-
+    var dat = {q:'1', m:'7', y:'2015'};
+    zeller(dat).should.equal('4');
   })
 })
