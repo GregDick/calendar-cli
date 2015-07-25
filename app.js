6bf27ee4d3fdcb1d8d46255498d120e0dceab1fd
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-07-25 15:15:44 -0500

    moved header into align function and moved the seperate app.js functions in to their own output function

--- app.js
+++ app.js
@@ -4,29 +4,38 @@ var month = require(process.cwd() + '/lib/month');
 var align = require(process.cwd() + '/lib/align');
 var reformat = require(process.cwd() + '/lib/reformat');
 var zeller = require(process.cwd() + '/lib/zeller');
-var header = require(process.cwd() + '/lib/header');
 
-if(process.argv.length > 2){
+//if only a year is passed
+if(process.argv.length===3){
+  //show year calendar
+}
+//if a month and year are passed
+else if(process.argv.length > 3){
   var m = process.argv[2];
   var y = process.argv[3];
 
-  console.log(header(month[m].name, y));
-  var roughDate = {
-    day: '1',
-    month: m,
-    year: y
-  }
-  var formatted = reformat(roughDate);
-  var weekday = zeller(formatted);
-  console.log(align(weekday, month[m].days));
+  console.log(output(1, m, y, month[m].days, month[m].name));
 }
 //if no arguments are passed
 else{
   var today = new Date();
   var d = today.getDate() + '';
   var m = today.getMonth() + 1 + '';
-  var y = today.getYear() + '';
-  console.log(month[m].name);
-  console.log(align(0, month[m].days));
+  var y = today.getFullYear() + '';
+
+  console.log(output(d, m, y, month[m].days, month[m].name));
+}
+
+//accepts the day month year, number of days in the month, and name of the month
+//compiles all of this information to a calendar format
+function output(d, m, y, days, name){
+  var roughDate = {
+    day: d,
+    month: m,
+    year: y
+  }
+  var formatted = reformat(roughDate);
+  var weekday = zeller(formatted);
+  return align(weekday, days, name, y);
 }
 
